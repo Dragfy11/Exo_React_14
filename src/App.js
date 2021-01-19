cfec1be9da32d563b11f54a4ebaf3a311ed3e6cb
Kadir <kadir.poyraz@outlook.be>
2021-01-19 14:26:11 +0100

    fin exo14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react'
+import Rich from './Components/Projet'
 import './App.css';
 
 let App=() => {
   const titre = useState("Exercice 14")
-    
+  const parag = useState("Div2")
   return (
     <div className="App">
       <h1>{titre}</h1>
+      <Rich para={parag}/>
+
     </div>
   );
 }
---
+++ src/Components/Projet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let projet = ({para}) => {
+
+    return (
+        <div>
+            <h2> mon paragraphe : {para}</h2>
+        </div>
+    )
+}
+
+export default projet
